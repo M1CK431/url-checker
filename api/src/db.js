e7514abe9bebc502cb3fecac2d21addb1bb2bf6d
M1CK431 <fontaine.mickael@laposte.net>
2025-08-02 01:45:37 +0200

    [API] Tune SQLite for better perfs

--- api/src/db.js
+++ api/src/db.js
@@ -9,6 +9,16 @@ try {
   await db.$connect();
   // eslint-disable-next-line no-console
   console.log("Connection has been established successfully.");
+
+  const sqliteConfig = [
+    "journal_mode = WAL",
+    "foreign_keys = ON",
+    "journal_size_limit = 6144000"
+  ];
+
+  await Promise.all(
+    sqliteConfig.map(cnf => db.$executeRawUnsafe(`PRAGMA ${cnf}`))
+  );
 } catch (error) {
   // eslint-disable-next-line no-console
   console.error("Unable to connect to the database.");
